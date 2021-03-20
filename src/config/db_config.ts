d72f64d0de7753ee429eefeb6663c7bafa4687d3
Junghoon Lee <jeonghunn1@gmail.com>
2021-03-20 20:17:48 +0900

    chore: Add a test database configuration file

---
+++ src/config/db_config.ts
@@ -0,0 +1,9 @@
+module.exports = (() => {
+    return {
+        host: "tarks.cjjyo4eg90ht.ap-northeast-2.rds.amazonaws.com",
+        user: "testing",
+        password: "unopenedboxtest0987",
+        database: "itboard_test",
+        dialect: "mariadb",
+    }
+})();
