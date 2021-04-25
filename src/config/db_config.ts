68188239206ec6973f0b19ef302ed396978baa06
Junghoon Lee <jeonghunn1@gmail.com>
2021-04-25 20:34:12 +0900

    chore: Fix database name for testing

--- src/config/db_config.ts
+++ src/config/db_config.ts
@@ -3,7 +3,7 @@ module.exports = (() => {
         host: "tarks.cjjyo4eg90ht.ap-northeast-2.rds.amazonaws.com",
         user: "testing",
         password: "unopenedboxtest0987",
-        database: "itboard_test",
+        database: "surreal_test",
         dialect: "mariadb",
     }
 })();
