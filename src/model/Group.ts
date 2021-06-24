b21f75c30398cbdb69debfe85134fbf19033d4c5
Junghoon Lee <jeonghunn1@gmail.com>
2021-06-24 23:10:11 +0900

    chore: Create a group model

---
+++ src/model/Group.ts
@@ -0,0 +1,60 @@
+import {
+    AllowNull,
+    AutoIncrement,
+    Column,
+    CreatedAt,
+    DataType,
+    ForeignKey,
+    Index,
+    Length,
+    Model,
+    PrimaryKey,
+    Table,
+    UpdatedAt,
+} from "sequelize-typescript";
+import {RoomStatus, Status} from "../core/type";
+import { User } from "./User";
+
+@Table
+export class Group extends Model {
+    @AutoIncrement
+    @PrimaryKey
+    @Column(DataType.INTEGER.UNSIGNED)
+    id!: number;
+
+    @Length({ max: 30 })
+    @Column(DataType.TEXT)
+    name!: string;
+
+    @AllowNull
+    @Length({ max: 300 })
+    @Column(DataType.TEXT)
+    description!: string;
+
+    @Index
+    @ForeignKey(() => User)
+    @Column({
+        type: DataType.BIGINT.UNSIGNED,
+        allowNull: false,
+    })
+    user_id!: number;
+
+    @Length({min: 0, max: 250})
+    @AllowNull
+    @Column(DataType.TEXT)
+    ip_address!: string;
+
+    @Column(DataType.INTEGER)
+    limit!: number;
+
+    @Column(DataType.INTEGER)
+    status!: Status;
+
+    @CreatedAt
+    createdAt!: Date;
+
+    @UpdatedAt
+    updatedAt!: Date;
+
+}
+
