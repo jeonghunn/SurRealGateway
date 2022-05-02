a32cd15991559982bf021efcd66034ef16741bfe
Junghoon Lee <jeonghunn1@gmail.com>
2022-05-03 06:24:01 +0900

    feat: Add a chat message list API

--- src/controller/ChatController.ts
+++ src/controller/ChatController.ts
@@ -1,77 +1,18 @@
-import { Room } from "../model/Room";
 import {
-    AttendeePermission,
-    AttendeeType,
-    AuthMessage,
-    ChatMessage,
-    SimpleUser,
     Status,
 } from "../core/type";
-import { AttendeeController } from "./AttendeeController";
+import { Chat } from "../model/Chat";
 import { Op } from "sequelize";
-import jwt from "jsonwebtoken";
-import { User } from "../model/User";
-import {getOptions} from "sequelize-typescript";
-import {Chat} from "../model/Chat";
 
-const config = require('../config/config');
+export class ChatController {
 
-export class RoomController {
-
-    public get(groupId: number, id: number): Promise<Room | null> {
-        return Room.findOne({
-                where: {
-                    status: Status.NORMAL,
-                    id,
-                    group_id: groupId,
-                },
-            }
-        ).catch((result) => {
-            console.log('Error: get from RoomController', result);
-            return null;
-        });
-
-    }
-
-    public create(
-        user_id: number,
-        group_id: number,
-        name?: string,
-        description?: string,
-        ip_address?: string,
-        limit: number = 10,
-
-    ): Promise<Room> {
-        const attendeeController: AttendeeController = new AttendeeController();
-
-        return Room.create({
-            user_id,
-            group_id,
-            name,
-            description,
-            ip_address,
-            limit,
-            status: Status.NORMAL,
-        }).then((room: Room) => {
-
-            attendeeController.create(
-                AttendeeType.ROOM,
-                user_id,
-                room.id,
-                AttendeePermission.ADMIN,
-            )
-
-            return room;
-        });
-    }
-
-
-    public getList(group_id: number, before: Date, offset: number = 0, limit: number = 15): Promise<Room[]> {
-        return Room.findAll(
+    public getList(roomId: number, before: Date, offset: number = 0, limit: number = 15): Promise<Chat[]> {
+        return Chat.findAll(
             {
+                attributes: ['id', 'category', 'content', 'user_id', 'room_id', 'status', 'createdAt', 'updatedAt'],
                 where: {
                     status: Status.NORMAL,
-                    group_id,
+                    room_id: roomId,
                     createdAt: {[Op.lte]: before},
                 },
                 order: [
@@ -83,33 +24,8 @@ export class RoomController {
         )
     }
 
-    public getVerifiedUser(authMessage: string): SimpleUser | null {
-        try {
-            const auth: AuthMessage = JSON.parse(authMessage);
-            const jwtInfo: any = jwt.verify(auth.token?.split(" ")[1]!, 'TEST_SERVER_SECRET');
-
-            return {
-                id: jwtInfo.id,
-                name: jwtInfo.name,
-            };
-        } catch (e: any) {
-            console.log('Error: getVerifiedUser from RoomController', e);
-            return null;
-        }
-    }
-
-    public parseChatMessage(content: string, me: SimpleUser): ChatMessage {
-        const chat: ChatMessage = JSON.parse(content);
-
-        chat.createdAt = new Date();
-
-        const user: User = new User();
-        user.name = me.name!;
-        user.id = me.id!;
-
-        chat.user = user;
-
-        return chat;
+    public async save(chat: Chat): Promise<Chat> {
+        return chat.save()
     }
 
 }
--- src/routes/room.ts
+++ src/routes/room.ts
@@ -15,6 +15,8 @@ import {
     AttendeeType,
 } from "../core/type";
 import { AttendeeController } from "../controller/AttendeeController";
+import { ChatController } from "../controller/ChatController";
+import { Chat } from "../model/Chat";
 
 const config = require('../config/config');
 const express = require('express');
@@ -70,6 +72,33 @@ router.get(
         });
 
     });
+
+
+router.get(
+    '/:id/chat',
+    util.validate([
+        param('group_id').isInt(),
+        query('offset').isInt(),
+        query('limit').isInt(),
+    ]),
+    jwt({ secret: config.jwt.secret, algorithms: config.jwt.algorithms }),
+    util.requirePermission(AttendeeType.GROUP, AttendeePermission.MEMBER),
+    (request: any, response: Response, next: NextFunction) => {
+        const chatController: ChatController = new ChatController();
+
+        const id: number = parseInt(request.params.id);
+        const offset: number = parseInt(request.query.offset);
+        const limit: number = parseInt(request.query.limit);
+        const before: Date = request.query.before ? new Date(parseInt(request.query.before) * 1000) : new Date();
+
+        chatController.getList(id, before, offset, limit).then((chats: Chat[]) => {
+            response.status(200).json({
+                chats,
+            });
+        });
+
+    });
+
 
 router.get(
     '/',
