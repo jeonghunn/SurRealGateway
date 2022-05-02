62aba3f97bfd804f7bb26b60cbdca8d5a17398f0
Junghoon Lee <jeonghunn1@gmail.com>
2022-05-03 06:25:44 +0900

    chore: Add a test for chat message list API

--- src/test/room.spec.ts
+++ src/test/room.spec.ts
@@ -1,7 +1,10 @@
-import { expect } from 'chai';
+import {expect} from 'chai';
 import request from 'supertest';
 import {TestUtil} from "./TestUtil";
-import {Test} from "mocha";
+import {ChatController} from "../controller/ChatController";
+import {Chat} from "../model/Chat";
+import {Status} from "../core/type";
+
 let app = require('../app');
 
 describe('Room', () => {
@@ -76,6 +79,54 @@ describe('Room', () => {
 
                     done();
                 });
+        });
+
+
+    });
+
+    it('Get Chat Message List', (done) => {
+        const testUtil: TestUtil = new TestUtil();
+        const chatController: ChatController = new ChatController();
+        const before: number = new Date().getTime() / 1000;
+        const chat: Chat = new Chat();
+
+
+
+
+
+
+        testUtil.signIn().end( (err, res) => {
+
+            console.log("EWFWEFWEFWEFWEF", res.body.user_id)
+
+            chat.content = "TEST"
+            chat.createdAt = new Date(new Date().getTime() - 30000);
+            chat.status = Status.NORMAL;
+            chat.room_id = roomId;
+            chat.user_id = res.body.user_id;
+            chat.save();
+
+
+            request(app)
+                .get(`/group/2/room/${roomId}/chat`)
+                .set('Authorization', `Bearer ${res.body.token}`)
+                .query({
+                    offset: 0,
+                    limit: 15,
+                    before: before,
+                })
+                .expect(200)
+                .end((err, res) => {
+                    if (err) {
+                        console.log(err, res);
+                        return;
+                    }
+
+                    expect(res.body.chats.length).greaterThan(0);
+
+                    done();
+                });
+
         });
 
 
