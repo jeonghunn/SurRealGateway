387eab292d4bbb9b09c576f6e5d3201d6c9fbd32
Junghoon Lee <jeonghunn1@gmail.com>
2022-05-04 20:59:46 +0900

    chore: Add a user name property to the result of chat history API

--- src/controller/ChatController.ts
+++ src/controller/ChatController.ts
@@ -1,19 +1,40 @@
 import {
     Status,
+    UserStatus,
 } from "../core/type";
 import { Chat } from "../model/Chat";
 import { Op } from "sequelize";
+import { User } from "../model/User";
 
 export class ChatController {
 
     public getList(roomId: number, before: Date, offset: number = 0, limit: number = 15): Promise<Chat[]> {
         return Chat.findAll(
             {
-                attributes: ['id', 'category', 'content', 'user_id', 'room_id', 'status', 'createdAt', 'updatedAt'],
+                attributes: [
+                    'id',
+                    'category',
+                    'content',
+                    'user_id',
+                    'room_id',
+                    'status',
+                    'createdAt',
+                    'updatedAt',
+                ],
                 where: {
                     status: Status.NORMAL,
                     room_id: roomId,
                     createdAt: {[Op.lte]: before},
+                },
+                include: {
+                    model: User,
+                    as: 'user',
+                    required: false,
+                    attributes: ['id', 'name'],
+                    where: {
+                        status: {[Op.ne]: UserStatus.REMOVED},
+                    }
+
                 },
                 order: [
                     ['id', 'DESC'],
