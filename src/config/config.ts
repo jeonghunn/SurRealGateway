c287094a1eff8313105f9bc5a426415a283eb158
Junghoon Lee <jeonghunn1@gmail.com>
2025-02-18 14:35:07 +0900

    chore: Do not hardcode the environment variables in the config and db_config

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,15 +1,32 @@
+import { credential } from "firebase-admin";
+
 module.exports = (() => {
     return {
-        serverDomain: 'surreal.io',
-        environment: 'development',
+        serverDomain: process.env.SERVER_DOMAIN,
+        environment: process.env.NODE_ENV || 'development',
         jwt: {
             algorithms: ['HS256'],
-            secret: 'TEST_SERVER_SECRET',
+            secret: process.env.JWT_SECRET,
         },
         attach: {
-            cdnUrl: 'http://localhost:3000/attach/',
-            path: '../attach/',
+            cdnUrl: '',
+            path: process.env.ATTACH_PATH,
             sizeLimit: 100 * 1024 * 1024,
         },
+        openAI: {
+            apiKey: process.env.OPENAI_API_KEY,
+        },
+        frontUrl: process.env.FRONT_URL,
+        aws: {
+            credentials: {
+                region: process.env.AWS_REGION,
+                credentials: {
+                    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+                    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+                },
+            },
+            bucket: process.env.AWS_S3_BUCKET_NAME,
+            
+        }
     }
 })();
